aa03b74c03ab3ed89f8a6477b29b6694a62357ed
aishwaryapatil22 <127470379+aishwaryapatil22@users.noreply.github.com>
2023-03-30 13:58:45 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,38 @@
+package assignment;
+public class Student {
+    private String name;
+    private int age;
+    private String collage;
+    
+    // Constructor with all parameters
+    public Student(String name, int age, String collage) {
+        this.name = name;
+        this.age = age;
+        this.collage = collage;
+    }
+    
+    // Getter and Setter methods 
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public String getcollage() {
+        return collage;
+    }
+    
+    public void setMajor(String collage) {
+        this.collage = collage;
+    }
+}
---
+++ StudentTest.java
@@ -0,0 +1,11 @@
+package assignment;
+
+class StudentTest {
+	public static void main(String[] args) {
+		
+	Student student1 = new Student("aishwarya", 25, "AGPIT");
+
+	System.out.println(student1.getName() + " is " + student1.getAge() + " years old, and the collage name is " + student1.getcollage() + ".");
+
+}
+}
