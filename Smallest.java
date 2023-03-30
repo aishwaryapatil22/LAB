d2ebc19140897512e4c63af6acc7bd85d6348fec
aishwaryapatil22 <127470379+aishwaryapatil22@users.noreply.github.com>
2023-03-30 13:26:44 +0530

    Add files via upload

---
+++ Smallest.java
@@ -0,0 +1,23 @@
+package assignment;
+
+public class Smallest {
+	 
+    public static void main(String[] args) {
+        //three numbers
+        int a = 20;
+        int b = 10;
+        int c = 30;
+         
+        int smallest;
+         
+        if(a<b && a<c) {
+            smallest = a;
+        } else if (b<c) {
+            smallest = b;
+        } else {
+            smallest = c;
+        }
+         
+        System.out.println(smallest + " is the smallest.");
+    }
+}
